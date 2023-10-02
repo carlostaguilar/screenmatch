9659fb0e431e33c5904ccac8d03d2c177b702394
Carlos Aguilar <taguilar2504@gmail.com>
2023-10-02 10:04:30 -0300

    Realizando a avaliação por temporada

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -9,10 +9,7 @@ import br.com.alura.screenmatch.service.ConverteDados;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Principal {
@@ -67,6 +64,19 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
+        System.out.print("Digite um trecho do titulo do episódio: ");
+        var trechoTitulo = sc.nextLine();
+
+        Optional<Episodio> episodioBuscado = episodios.stream()
+                                                      .filter(e -> e.getTitulo().toUpperCase().contains(trechoTitulo.toUpperCase()))
+                                                      .findFirst();
+        if(episodioBuscado.isPresent()) {
+            System.out.println("Episódio encontrado!");
+            System.out.println("Temporada: " + episodioBuscado.get().getTemporada());
+        } else {
+            System.out.println("Episódio não encontrado");
+        }
+
         System.out.println("A partir de que ano você deseja ver os episódios? ");
         var ano = sc.nextInt();
         sc.nextLine();
@@ -80,6 +90,13 @@ public class Principal {
                          "Temporada: " + e.getTemporada() +
                          " Episódio: " + e.getTitulo() +
                          " Data de Lançamento: " + e.getDataDeLancamento().format(formatter)));
+
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                                                               .filter(e -> e.getAvaliacao() > 0.0)
+                                                               .collect(Collectors.groupingBy(Episodio::getTemporada,
+                                                                        Collectors.averagingDouble(Episodio::getAvaliacao)));
+
+        System.out.println(avaliacoesPorTemporada);
 
     }
 
